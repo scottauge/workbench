4171c007923137ce77d4632ec8f9daf9155a4c70
Scott Auge <sauge@amduus.com>
2023-03-09 02:38:14 -0500

    Added save and load

--- workbench/Workbench.php
+++ workbench/Workbench.php
@@ -5,6 +5,7 @@ $Screen = "New";
 
 // Code to test PHP in a screen
 
+
 if (isset($_REQUEST["Code"])) {
 	$Code = $_REQUEST["Code"];
 	$Screen="";
@@ -12,6 +13,28 @@ if (isset($_REQUEST["Code"])) {
 else {
 	$Code = "";
     $Screen="New";
+}
+ 
+// Save
+
+if (isset($_REQUEST["button"]) && $_REQUEST["button"] == "Save") {
+	//echo $_REQUEST["button"];
+	$diskfile = $_REQUEST["file"];
+	echo "Writing file " . $diskfile;
+	$Handle=fopen($diskfile, "w");
+	fwrite($Handle, $Code);
+	fclose($Handle);
+}
+
+// Load
+
+if (isset($_REQUEST["button"]) && $_REQUEST["button"] == "Load") {
+	//echo $_REQUEST["button"];
+	$diskfile = $_REQUEST["file"];
+	echo "Reading file " . $diskfile;
+	$Handle=fopen($diskfile, "r");
+	$Code = fread($Handle, 1024);
+	fclose($Handle);
 }
 
 /*
@@ -37,12 +60,16 @@ if (isset($_REQUEST["Action"]))
 	Programming
     <form name="" action="" method="GET" autocomplete="off" novalidate="novalidate">
 	<textarea name="Code" rows="20" cols="80" id="Code"><?php echo $Code ?></textarea><br>
-    <input type="submit" value="Run"><input type="button" value="Clear" onclick="x=document.getElementById('Code');x.value=''"> 
+    <input type="submit" value="Run">
+	<input type="button" value="Clear" onclick="x=document.getElementById('Code');x.value=''">File Name:
+	<input type="text" id="file" name="file" <?php echo "value=\"$diskfile\"" ?>>
+	<input type="submit" name="button" value="Load">
+	<input type="submit" name="button" value="Save">
     </form>
     <p><br>
     </p>
     <?php 
-    if (isset($Code)) eval($Code)
+    if (isset($Code)) eval($Code);
     ?>
   </body>
   <?php
